508d256588e45ee393d3d2605b0d2a235dc70241
Yurii <yuriidaniltsiv@gmail.com>
2026-04-01 17:28:57 +0300

    add profit visualization and images

--- src/visualize.py
+++ src/visualize.py
@@ -26,12 +26,24 @@ dm["expense"] = dm["expense"].fillna(0)
 dm = dm.reset_index()
 dm["month"] = dm["month"].dt.to_timestamp()
 
-# Draw a graph
+# Draw a graph "Revenue & Expense by Month"
 plt.plot(dm["month"], dm["expense"], color="blue", label="Expense")
 plt.plot(dm["month"], dm["revenue"], color="red", label="Revenue")
 plt.xlabel("Date")
 plt.ylabel("Amount")
 plt.title("Revenue & Expense by month")
+plt.xticks(rotation=45)
+plt.legend()
+plt.show()
+
+# Calculate profit
+dm["profit"] = dm["revenue"] - dm["expense"]
+
+# Draw a graph "Profit by Month"
+plt.plot(dm["month"], dm["profit"], color="green", label="Profit")
+plt.xlabel("Date")
+plt.ylabel("Profit")
+plt.title("Profit by month")
 plt.xticks(rotation=45)
 plt.legend()
 plt.show()
